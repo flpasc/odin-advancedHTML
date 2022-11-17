b35de14075a1886b36505caa2cd74e3eeaed5c0d
Florian <florianpaulschoene@gmail.com>
2022-11-17 21:45:47 +0100

    working not dynamic

--- forms/Sign Up Page/signup.js
+++ forms/Sign Up Page/signup.js
@@ -1,15 +1,16 @@
 var checkpwd = function () {
 	const pwd = document.getElementById("pwd").value;
 	const rptpwd = document.getElementById("rptpwd").value;
-	const message = document.getElementById("tooltip");
 	const checkmark = document.getElementById("checkmark");
 
 	if (pwd === rptpwd) {
 		checkmark.innerHTML = "√";
 		checkmark.style.color = "green";
+		console.log("pwd = rptpwd");
 	} else {
 		checkmark.innerHTML = "X";
 		checkmark.style.color = "red";
 		checkmark.style.fontSize = "20px";
+		console.log("else");
 	}
 };
